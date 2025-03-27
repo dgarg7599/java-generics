f6ef1abc794a721b1452a62b3fe08547cea38866
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-27 11:45:55 +0530

    Generic class for flights.

---
+++ com/bridgelabz/flightScheduling/Flight.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.flightScheduling;
+
+public class Flight<T> {
+    private T flightNumber;
+    private String departure;
+    private String destination;
+
+    public Flight(T flightNumber, String departure, String destination) {
+        this.flightNumber = flightNumber;
+        this.departure = departure;
+        this.destination = destination;
+    }
+
+    public T getFlight() {
+        return flightNumber;
+    }
+
+    public void displayFlightInfo(){
+        System.out.println("Flight: " + flightNumber + " | From: " + departure + " | To: " + destination);
+    }
+}
