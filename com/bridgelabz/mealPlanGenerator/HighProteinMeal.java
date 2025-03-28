409d15897f554139c60b5e711670f4cafeda59cb
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-28 09:52:05 +0530

    [Divyansh Garg] ADD: HighProtienMeal class extending MealPlan.

---
+++ com/bridgelabz/mealPlanGenerator/HighProteinMeal.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.mealPlanGenerator;
+
+public class HighProteinMeal implements MealPlan {
+    @Override
+    public String getMealType() {
+        return "High-Protein Meal";
+    }
+}
