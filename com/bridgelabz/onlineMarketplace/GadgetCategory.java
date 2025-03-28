3ababec7cb62936caf128d95ceef3ceb99e7007c
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-28 11:34:19 +0530

    [Divyansh Garg] ADD: Added GadgetCategory class for product categorization

---
+++ com/bridgelabz/onlineMarketplace/GadgetCategory.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.onlineMarketplace;
+
+class GadgetCategory implements ProductCategory {
+    @Override
+    public String getCategoryName() {
+        return "Gadgets";
+    }
+}
