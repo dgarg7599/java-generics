18aa081b6d920cadb591df547d7967e4a3fa5cee
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-28 11:38:21 +0530

    [Divyansh Garg] ADD: Created SoftwareEngineer class for job role categorization

---
+++ com/bridgelabz/resumeScreeningSystem/SoftwareEngineer.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.resumeScreeningSystem;
+
+public class SoftwareEngineer extends JobRole {
+    @Override
+    public String getRoleName() {
+        return "Software Engineer";
+    }
+}
