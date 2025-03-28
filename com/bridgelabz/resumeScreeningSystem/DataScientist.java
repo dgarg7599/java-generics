713ba2685c17c66590433cd9f49f8f6af7059d60
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-28 11:38:18 +0530

    [Divyansh Garg] ADD: Created DataScientist class for job role categorization

---
+++ com/bridgelabz/resumeScreeningSystem/DataScientist.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.resumeScreeningSystem;
+
+public class DataScientist extends JobRole {
+    @Override
+    public String getRoleName() {
+        return "Data Scientist";
+    }
+}
