26a82d7dce37aeb18fbfc267a873f59ff3aa97d7
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-28 11:34:19 +0530

    [Divyansh Garg] ADD: Added ClothingCategory class for product categorization

---
+++ com/bridgelabz/onlineMarketplace/ClothingCategory.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.onlineMarketplace;
+
+class ClothingCategory implements ProductCategory {
+    @Override
+    public String getCategoryName() {
+        return "Clothing";
+    }
+}
