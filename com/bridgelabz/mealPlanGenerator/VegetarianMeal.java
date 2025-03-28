243844ea5b4b2d7e4d6769955f786c6d582c570e
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-28 09:56:23 +0530

    [Divyansh Garg] ADD: VegetarianMeal class implements MealPlan.

---
+++ com/bridgelabz/mealPlanGenerator/VegetarianMeal.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.mealPlanGenerator;
+
+public class VegetarianMeal implements MealPlan {
+    @Override
+    public String getMealType() {
+        return "Vegetarian Meal";
+    }
+}
